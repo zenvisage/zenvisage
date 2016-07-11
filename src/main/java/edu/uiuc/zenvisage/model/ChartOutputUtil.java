a3dc606a0fbf3b45d37069cba873c2031320db0f
Chaoran <superchaoran@gmail.com>
2016-07-11 17:15:18 -0500

    fix x-axis, omit rank value

--- src/main/java/edu/uiuc/zenvisage/model/ChartOutputUtil.java
+++ src/main/java/edu/uiuc/zenvisage/model/ChartOutputUtil.java
@@ -50,7 +50,9 @@ public class ChartOutputUtil {
 			int j = 0;
 			for (double[][] result : output) {
 				Chart chartOutput = new Chart();
-				chartOutput.setxType((i+1)+" : "+mappings.get(orders.get(i)));
+				/*Separate this call to rank and x axix and return separately*/
+				//chartOutput.setxType((i+1)+" : "+mappings.get(orders.get(i)));
+				chartOutput.setxType(mappings.get(orders.get(i)));
 				chartOutput.setyType(args.getSketchPoints()[j].aggrFunc+"("+args.getSketchPoints()[j].yAxis+")");
 				// fill in chart data
 				String key = mappings.get(orders.get(i));
@@ -77,7 +79,10 @@ public class ChartOutputUtil {
 			// initialize a new chart
 			Chart chartOutput = new Chart();
 			RepresentativeTrend repTrend = representativeTrends.get(i);
-			chartOutput.setxType((i+1)+" : "+repTrend.getKey());
+			/*Separate this call to rank and x axix and return separately*/
+			//chartOutput.setxType((i+1)+" : "+repTrend.getKey());
+			chartOutput.setxType(repTrend.getKey());
+			
 			chartOutput.setyType(args.aggrFunc+"("+args.yAxis+")");
 			// fill in chart data
 			LinkedHashMap<Float,Float> points = orig.get(repTrend.getKey());
