d1ff8f6c5c0fb0d907b925983d457f862b9ab207
Tarique <tarique.nitk@gmail.com>
2016-10-10 13:46:28 -0500

    axis variable

--- src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/AxisVariable.java
+++ src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/AxisVariable.java
@@ -3,12 +3,15 @@
  */
 package edu.uiuc.zenvisage.zqlcomplete.querygraph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author tarique
  *
  */
 public class AxisVariable {
 	private String type;
-	private values=""
+	private List<String> values=new ArrayList<String>();
 	
 }
