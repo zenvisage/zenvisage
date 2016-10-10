fbab9e89c4aeb6090452f66a6cbf8eb13cea8811
Tarique <tarique.nitk@gmail.com>
2016-10-10 13:02:00 -0500

    query exec

---
+++ src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/LookUpTable.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.uiuc.zenvisage.zqlcomplete.querygraph;
+
+import java.util.HashMap;
+
+/**
+ * @author tarique
+ *
+ */
+public class LookUpTable {
+	private HashMap<String, Object> variables= new HashMap<String,Object>();
+	public LookUpTable(){	}
+	public void put(String name, Object obj){
+		variables.put(name,obj);
+	}
+	public Object get(String name){
+		return variables.get(name);
+	}
+
+}
