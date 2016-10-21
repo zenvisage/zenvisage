bed6993c6602930db88b67a1282ef9340cb35a37
Edward Xue <edthemaster1@gmail.com>
2016-10-21 10:49:29 -0500

    Setup basic query test so we don't have to go through build process to test

--- src/test/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/test/GraphExecutionTest.java
+++ src/test/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/test/GraphExecutionTest.java
@@ -1,5 +1,28 @@
 package edu.uiuc.zenvisage.zqlcomplete.querygraph.test;
 
+import java.io.IOException;
+
+import org.junit.Test;
+
+import edu.uiuc.zenvisage.service.ZvMain;
+
 public class GraphExecutionTest {
 
+	@Test
+	public void TestBasicQueryExecution() {
+		String arg = "{\"zqlRows\":[{\"name\":{\"output\":false,\"sketch\":false,\"name\":\"f1\"},\"x\":{\"values\":[\"'year'\"]},\"y\":{\"values\":[\"'soldprice'\"]},\"z\":{\"column\":\"'state'\",\"values\":[\"*\"]}}]}";
+
+		try {
+			ZvMain zvMain = new ZvMain();
+			System.out.println("testing");
+			String outputGraphExecutor = zvMain.runQueryGraph(arg);
+			System.out.println("Output");
+			System.out.println(outputGraphExecutor);
+			
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
