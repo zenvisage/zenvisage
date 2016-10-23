7753cf343329e86c6b38b39d02e25615c58c6696
Sean Zou <yzouac@connect.ust.hk>
2016-10-23 15:10:40 +0800

    implemented sortFilters

--- src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/SortFilterPrimitive.java
+++ src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/SortFilterPrimitive.java
@@ -3,6 +3,10 @@
  */
 package edu.uiuc.zenvisage.zqlcomplete.querygraph;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author tarique
  *
@@ -11,4 +15,22 @@ public interface SortFilterPrimitive {
 
 	public AxisVariableScores execute(AxisVariableScores fpo, int count);
 	
+	public class ScoreAndIndex {
+		double score;
+		int index;
+		ScoreAndIndex(double s, int i) {
+			this.score = s;
+			this.index = i;
+		}
+	}
+	
+	public class SIComparator implements Comparator<ScoreAndIndex> {
+		double maxOrMin;
+		SIComparator(int maxOrMin) {
+			this.maxOrMin = maxOrMin;
+		}
+		public int compare(ScoreAndIndex si1, ScoreAndIndex si2) {
+			return (int) ((maxOrMin ) * (si1.score - si2.score));
+		}
+	}
 }
