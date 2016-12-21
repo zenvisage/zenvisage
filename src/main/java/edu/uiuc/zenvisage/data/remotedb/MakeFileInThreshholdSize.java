095b2d32dd98143a72e78a5fdede17bef9396653
Chaoran <superchaoran@gmail.com>
2016-12-20 18:11:53 -0800

    script to process flights dataset

--- src/main/java/edu/uiuc/zenvisage/data/remotedb/MakeFileInThreshholdSize.java
+++ src/main/java/edu/uiuc/zenvisage/data/remotedb/MakeFileInThreshholdSize.java
@@ -7,9 +7,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ConvertRawToCSV {
-	private static final String READFILENAME = "data/flight-random200k.csv";
+public class MakeFileInThreshholdSize {
+	private static final String READFILENAME = "data/flight-random2M.csv";
 	private static final String WriteFILENAME ="data/flights.csv";
+	private static final int N = 2000000;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = null;
@@ -24,7 +25,8 @@ public class ConvertRawToCSV {
 			
 		String sCurrentLine;
 		bw.write("year,month,day,weekday,carrier,origin,destination,arrivaldelay,departuredelay,weatherdelay,distance\n");
-		while ((sCurrentLine = br.readLine()) != null) {
+		int n = 0;
+		while ((sCurrentLine = br.readLine()) != null && n++ < N) {
 			String[] sArr = sCurrentLine.split(",");
 			//7,8,9,arrivaldelay,departuredelay,weatherdelay
 			if (Integer.parseInt(sArr[7]) < 0 || Integer.parseInt(sArr[8]) < 0 || Integer.parseInt(sArr[9]) < 0 || Integer.parseInt(sArr[10]) < 0){
@@ -32,9 +34,11 @@ public class ConvertRawToCSV {
 			}			
 			bw.write(sCurrentLine+"\n");
 		}
+		
 		br.close();
 		fr.close();
 		bw.close();
 		fw.close();
+		System.out.println("Completed!");
 	}
 }
