b2838abeffecbb024726b59e76331f56f8dffa74
eidehua <edthemaster1@gmail.com>
2017-01-07 03:01:12 -0500

    Updated DEuclidean to support more generic axisvariable queries, so users don't have to specify cross product for one vs many cases

--- src/test/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/test/GraphExecutionTest.java
+++ src/test/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/test/GraphExecutionTest.java
@@ -103,7 +103,34 @@ public class GraphExecutionTest {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Test
+	// cross product query, but testing v2<-argmax_{z2} instead of v1,v2<-argmax_{z1,z2}
+	public void TestOnevsManyQueryExecution() {
+		//Q2
+		//String arg = "{\"zqlRows\":[{\"name\":{\"output\":false,\"sketch\":false,\"name\":\"f1\"},\"x\":{\"variable\":\"x1\",\"values\":[\"'year'\"]},\"y\":{\"variable\":\"y1\",\"values\":[\"'soldprice'\"]},\"z\":{\"variable\":\"z1\",\"attribute\":\"'state'\",\"values\":[\"*\"]},\"constraints\":[{\"key\":\"state\",\"operator\":\"=\",\"value\":\"'CA'\"}]},{\"name\":{\"output\":false,\"sketch\":false,\"name\":\"f2\"},\"x\":{\"variable\":\"x1\",\"values\":[]},\"y\":{\"variable\":\"y1\",\"values\":[]},\"z\":{\"variable\":\"z1\",\"values\":[]},\"processe\":{\"variables\":[\"v1\"],\"method\":\"Dissimilar\",\"count\":\"7\",\"metric\":\"D\",\"arguments\":[\"f1\",\"f2\"]}},{\"name\":{\"output\":true,\"sketch\":false,\"name\":\"f3\"},\"x\":{\"variable\":\"x1\",\"values\":[]},\"y\":{\"variable\":\"y1\",\"values\":[]},\"z\":{\"variable\":\"v1\",\"values\":[]}}]}";
+		// attribute can be null or empty string
+		//String arg = "{\"zqlRows\":[{\"name\":{\"name\":\"f1\",\"output\":false,\"sketch\":false},\"x\":{\"variable\":\"x1\",\"attribute\":\"year\",\"values\":[\"'year'\"]},\"y\":{\"variable\":\"y1\",\"attribute\":\"soldprice\",\"values\":[\"'soldprice'\"]},\"z\":{\"variable\":\"z1\",\"attribute\":\"state\",\"values\":[\"*\"],\"expression\":\"\"},\"processe\":{\"variables\":[],\"method\":\"\",\"axis\":[],\"count\":\"\",\"metric\":\"\",\"arguments\":[],\"parameters\":{}},\"viz\":{\"variable\":\"\",\"type\":[],\"parameters\":[]},\"sketchPoints\":null,\"constraint\":[{\"key\":\"state\",\"operator\":\"=\",\"value\":\"'CA'\"}],\"output\":false},{\"name\":{\"name\":\"f2\",\"output\":false,\"sketch\":false},\"x\":{\"variable\":\"x1\",\"attribute\":null,\"values\":[]},\"y\":{\"variable\":\"y1\",\"attribute\":null,\"values\":[]},\"z\":{\"variable\":\"z1\",\"attribute\":\"\",\"values\":[],\"expression\":\"\"},\"processe\":{\"variables\":[\"v1\"],\"method\":\"Dissimilar\",\"axis\":[],\"count\":\"7\",\"metric\":\"D\",\"arguments\":[\"f1\",\"f2\"],\"parameters\":{}},\"viz\":{\"variable\":\"\",\"type\":[],\"parameters\":[]},\"sketchPoints\":null,\"constraint\":[],\"output\":false},{\"name\":{\"name\":\"f3\",\"output\":true,\"sketch\":false},\"x\":{\"variable\":\"x1\",\"attribute\":null,\"values\":[]},\"y\":{\"variable\":\"y1\",\"attribute\":null,\"values\":[]},\"z\":{\"variable\":\"v1\",\"attribute\":\"\",\"values\":[],\"expression\":\"\"},\"processe\":{\"variables\":[],\"method\":\"\",\"axis\":[],\"count\":\"\",\"metric\":\"\",\"arguments\":[],\"parameters\":{}},\"viz\":{\"variable\":\"\",\"type\":[],\"parameters\":[]},\"sketchPoints\":null,\"constraint\":[],\"output\":true}]}";
+		String arg = "{\"db\":\"real_estate\", \"zqlRows\":[{\"name\":{\"output\":false,\"sketch\":false,\"name\":\"f1\"},\"x\":{\"variable\":\"x1\",\"attributes\":[\"'year'\"]},\"y\":{\"variable\":\"y1\",\"attributes\":[\"'soldprice'\"]},\"z\":{\"variable\":\"z1\",\"attribute\":\"'state'\",\"values\":[\"*\"]},\"constraints\":[{\"key\":\"state\",\"operator\":\"=\",\"value\":\"'CA'\"}]},{\"name\":{\"output\":false,\"sketch\":false,\"name\":\"f2\"},\"x\":{\"variable\":\"x1\",\"attributes\":[]},\"y\":{\"variable\":\"y1\",\"attributes\":[]},\"z\":{\"variable\":\"z2\",\"attribute\":\"'state'\",\"values\":[\"*\"]},\"processe\":{\"variables\":[\"v2\"],\"method\":\"DEuclidean\",\"count\":\"3\",\"metric\":\"argmax\",\"arguments\":[\"f1\",\"f2\"],\"axisList1\":[\"z2\"],\"axisList2\":[]}},{\"name\":{\"output\":true,\"sketch\":false,\"name\":\"f3\"},\"x\":{\"variable\":\"x1\",\"attributes\":[]},\"y\":{\"variable\":\"y1\",\"attributes\":[]},\"z\":{\"variable\":\"v2\",\"values\":[]}}]}";
+				
+		try {
+			ZvMain zvMain = new ZvMain();
+			System.out.println("testing crossproduct process query");
+			String outputGraphExecutor = zvMain.runQueryGraph(arg);
+			System.out.println("Cross Product Output");
+			System.out.println(outputGraphExecutor);
+			System.out.println(outputGraphExecutor.length());
+			
+			System.out.println("old stuff");
+			//String outputOldExecutor = zvMain.runZQLCompleteQuery(arg);
+			System.out.println("Old Output");
+			//System.out.println(outputOldExecutor);
+			
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
 	@Test
 	public void TestPairwiseProcessQueryExecution() {
 		//Q5
