cbec9e48ab51878bff83028f73e613fd27a5b062
eidehua <edthemaster1@gmail.com>
2017-01-07 03:00:34 -0500

    Updated windows run script

--- run-win.sh
+++ run-win.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 pskill -f ZvServer
 cd target
-java -cp "zenvisage/WEB-INF/lib/*;zenvisage-jar-with-dependencies.jar" edu.uiuc.zenvisage.server.ZvServer
+java -cp "zenvisage/WEB-INF/lib/*;zenvisage-jar-with-dependencies.jar;classes/data" edu.uiuc.zenvisage.server.ZvServer
