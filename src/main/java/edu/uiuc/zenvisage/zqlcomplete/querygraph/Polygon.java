3cd5888da4adbbea6b4e1e58fadf6befbbbb8079
Edward Xue <edthemaster1@gmail.com>
2017-05-08 02:37:48 -0500

    Update polygon to support a list of points. Updated inArea function to do the bounding box calculation

--- src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/Polygon.java
+++ src/main/java/edu/uiuc/zenvisage/zqlcomplete/querygraph/Polygon.java
@@ -1,30 +1,63 @@
 package edu.uiuc.zenvisage.zqlcomplete.querygraph;
 
+import java.util.List;
+
+import edu.uiuc.zenvisage.model.Point;
 import edu.uiuc.zenvisage.model.ScatterResult.Tuple;
 
 public class Polygon {
-	public double x1;
-    public double x2;
-    public double y1;
-    public double y2;
+	
+	private List<Point> points;
+
 	/**
 	 * @param x1
 	 * @param x2
 	 * @param y1
 	 * @param y2
 	 */
-	public Polygon(double x1, double x2, double y1, double y2) {
-		this.x1 = x1;
-		this.x2 = x2;
-		this.y1 = y1;
-		this.y2 = y2;
+	public Polygon(List<Point> points) {
+		this.points = points;
 	}
 	
 	public Polygon() {
 		
 	}
 	
+	/**
+	 * Checks if a tuple is within the polygon
+	 * @param tuple
+	 * @return
+	 */
 	public boolean inArea(Tuple tuple) {
-		return this.x1 <= tuple.x && tuple.x <= this.x2 && this.y1 <= tuple.y && tuple.y <= this.y2;
+		if (points.isEmpty()) {
+			return false;
+		}
+		Point firstPoint = points.get(0);
+		float minX = firstPoint.getX();
+		float maxX = firstPoint.getX();
+		float minY = firstPoint.getY();
+		float maxY = firstPoint.getY();
+		
+		for (Point point : points) {
+			float x = point.getX();
+			float y = point.getY();
+			
+			if (x < minX) minX = x;
+			else if (x > maxX) maxX = x;
+			
+			if (y < minY) minY = y;
+			else if (y > maxY) maxY = y;
+		}
+		
+		float x = (float) tuple.x;
+		float y = (float) tuple.y;
+		
+		
+		// check if point is outside bounding box
+		if (x < minX || x > maxX || y < minY || y > maxY) {
+			return false;
+		}
+		
+		return false;
 	}
 }
