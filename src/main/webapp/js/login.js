5bcd77901346b793a3d6af66ff11bfe74dabba31
dorislee0309 <dorislee@berkeley.edu>
2017-07-29 16:18:02 -0500

    bugfix login

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -2,5 +2,6 @@ $("#loginDialog").modal('toggle');
 $("#signIn").on('click', function() {
     if (($("#username").val() === secret["username"]) & ($("#password").val() === secret["password"])){
     	$("#loginDialog").modal('hide');
+    	$("#loginModal").remove()
 	}
 });
