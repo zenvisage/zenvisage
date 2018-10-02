55df971254947e33611b50579e65f0ea696660ce
dorisjlee <dorisjunglinlee@gmail.com>
2018-10-02 12:49:54 -0500

    injecting equation working , but page not loading; deleted the mobile popup page size issue

--- src/main/webapp/spec.js
+++ src/main/webapp/spec.js
@@ -1,18 +1,20 @@
 describe('Zenvisage', function() {
-  it('should have a title', function() {
-    browser.get('http://localhost:80/');
-    expect(browser.getTitle()).toEqual('Zenvisage');
-  });
+  // it('should have a title', function() {
+  //   browser.get('http://localhost:80/');
+  //   expect(browser.getTitle()).toEqual('Zenvisage');
+  // });
 
   it('should be Louisville', function() {
-// implicit and page load timeouts
-    browser.manage().timeouts().pageLoadTimeout(400000);
-    browser.manage().timeouts().implicitlyWait(250000);
+    // implicit and page load timeouts
+    browser.manage().timeouts().pageLoadTimeout(4000000);
+    browser.manage().timeouts().implicitlyWait(2500000);
+
+    browser.driver.manage().timeouts().implicitlyWait(50000);
     browser.get('http://localhost:80/');
     element(by.model('equation')).sendKeys("y=x^2");
     element(by.xpath('//button[. = "add"]')).click();
-    // expect(element(by.id('row-0')).getText()).
-    // toEqual('city: Louisville (0.43)'); // This is wrong!
+    expect(element(by.id('row-0')).getText()).
+      toEqual('city: Louisville (0.43)'); // This is wrong!
     expect(browser.getTitle()).toEqual('Zenvisage');
   });
 });
