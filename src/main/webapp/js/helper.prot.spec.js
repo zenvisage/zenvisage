6e8dd6a44f727c7ac27b20f57382066708affbf5
dorisjlee <dorisjunglinlee@gmail.com>
2018-10-02 11:39:40 -0500

    added helper prot spec

--- src/main/webapp/js/helper.prot.spec.js
+++ src/main/webapp/js/helper.prot.spec.js
@@ -1,5 +1,9 @@
 describe('Zenvisage', function() {
-  it('should have a title', function() {
+  // // Before each test load our api.users module
+  // beforeEach(angular.mock.module('zenvisage'));
+  
+  browser.driver.manage().timeouts().implicitlyWait(50000);
+  it('Title Equality Test', function() {
     
     // implicit and page load timeouts
     browser.manage().timeouts().pageLoadTimeout(400000);
@@ -8,4 +12,28 @@ describe('Zenvisage', function() {
     browser.get('http://127.0.01:80/');
     expect(browser.getTitle()).toEqual('Zenvisage');
   });
+  // it('Representative Query Test', function() {
+    
+  //   // implicit and page load timeouts
+  //   browser.manage().timeouts().pageLoadTimeout(400000);
+  //   browser.manage().timeouts().implicitlyWait(250000);
+
+  //   browser.get('http://127.0.01:80/');
+
+  //   var firstRankResult;
+  //   var draggedGraph = uploadToSketchpadNew( "representative-result-0", "representativeQuery")
+  //   function uploadToSketchpadNewCallback(callback){
+  //       uploadToSketchpadNew( "representative-result-0", "representativeQuery")
+  //       callback()
+  //   }
+  //   function callback(){
+  //      firstRankResult= userQueryDygraphsNew["result-0"]["data"]
+  //   }
+  //   console.log(draggedGraph)
+  //   console.log(firstRankResult)
+  //   var draggedGraph = uploadToSketchpadNewCallback(callback)
+
+  //   // expect(arr).toEqual([[[1,2][2,2]],[[1,3][2,3]]]);
+  //   expect(draggedGraph).toEqual(firstRankResult);
+  // });
 });
--- src/main/webapp/js/helper.spec.js
+++ src/main/webapp/js/helper.spec.js
@@ -1,6 +1,6 @@
 describe('Zenvisage', function() {
   var Users;
-
+  
   // Before each test load our api.users module
   beforeEach(angular.mock.module('zenvisage'));
 
@@ -14,22 +14,22 @@ describe('Zenvisage', function() {
     // expect(arr).toEqual([1,2,3]);
   });
 
-  it('should succeed', function() {
-    var firstRankResult;
-    var draggedGraph = uploadToSketchpadNew( "representative-result-0", "representativeQuery")
-    function uploadToSketchpadNewCallback(callback){
-        uploadToSketchpadNew( "representative-result-0", "representativeQuery")
-        callback()
-    }
-    function callback(){
-       firstRankResult= userQueryDygraphsNew["result-0"]["data"]
-    }
-    console.log(draggedGraph)
-    console.log(firstRankResult)
-    var draggedGraph = uploadToSketchpadNewCallback(callback)
+  // it('should succeed', function() {
+  //   var firstRankResult;
+  //   var draggedGraph = uploadToSketchpadNew( "representative-result-0", "representativeQuery")
+  //   function uploadToSketchpadNewCallback(callback){
+  //       uploadToSketchpadNew( "representative-result-0", "representativeQuery")
+  //       callback()
+  //   }
+  //   function callback(){
+  //      firstRankResult= userQueryDygraphsNew["result-0"]["data"]
+  //   }
+  //   console.log(draggedGraph)
+  //   console.log(firstRankResult)
+  //   var draggedGraph = uploadToSketchpadNewCallback(callback)
 
-    // expect(arr).toEqual([[[1,2][2,2]],[[1,3][2,3]]]);
-    expect(draggedGraph).toEqual(firstRankResult);
-    // expect(arr).toEqual([1,2,3]);
-  });
+  //   // expect(arr).toEqual([[[1,2][2,2]],[[1,3][2,3]]]);
+  //   expect(draggedGraph).toEqual(firstRankResult);
+  //   // expect(arr).toEqual([1,2,3]);
+  // });
 });
