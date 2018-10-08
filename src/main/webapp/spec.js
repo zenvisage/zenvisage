ff56ef408cfa18e55b2f09343b910c3a5ed54ab4
dorisjlee <dorisjunglinlee@gmail.com>
2018-10-08 15:37:48 -0500

    added document ready script to test itself to resolve page loading issue

--- src/main/webapp/spec.js
+++ src/main/webapp/spec.js
@@ -5,7 +5,7 @@ describe('Zenvisage', function() {
   // });
 
   it('DragNDrop should be Louisville', function() {
-    debugger;
+    // debugger;
     // implicit and page load timeouts
     browser.manage().timeouts().pageLoadTimeout(4000000);
     browser.manage().timeouts().implicitlyWait(2500000);
@@ -13,24 +13,15 @@ describe('Zenvisage', function() {
     browser.driver.manage().timeouts().implicitlyWait(50000);
     browser.get('http://localhost:80/');
 
-    // browser.executeAsyncScript(function (){
+    browser.executeScript("$('#dataset-form-control option').val('real_estate');var scope = angular.element('#dataset-form-control').scope();scope.onDatasetChange();")
+
     element(by.model('equation')).sendKeys("y=x^2");
     element(by.xpath('//button[. = "add"]')).click();
 
-    browser.pause();
     element(by.css('text[count="1"]')).getText().then(function(text){
-      console.log(text)
+      expect(text).toEqual('city: Plymouth (0.383)');
     });
-    // });
-
-    //   toEqual('city: Louisville (0.43)'); 
-    // });
-    // .
-    //   toEqual('city: Louisville (0.43)'); // This is wrong!
-    // });
-    // expect(element(by.id('row-0')).getText()).
-    //   toEqual('city: Louisville (0.43)'); // This is wrong!
-    // });
+    
   });
 });
 
