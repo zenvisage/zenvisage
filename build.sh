852e89a578f6405bb223694ff57393c1ae42cbe7
Jaewoo Kim <jaewookim@host-5-52.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-10-15 18:27:58 -0500

    adding travis files to default branch master

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!bin/bash
 mvn clean
+if [[ "$?" -ne 0 ]] ; then
+  echo 'mvn clean failed. Exited with non-zero value';exit 42
+fi
 mvn install
+if [[ "$?" -ne 0 ]] ; then
+  echo 'mvn install failed. Exited with non-zero value '; exit 42
+fi
 cp lib/*.jar target/zenvisage/WEB-INF/lib/.
