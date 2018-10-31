441ccea5a54d494d84bf9baa509d0dd8e6a7ab7b
Jaewoo Kim <jaewookim@host-5-48.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-10-31 17:53:21 -0500

    changed yaml

--- travis.sh
+++ travis.sh
@@ -6,11 +6,19 @@ mvn install
 if [[ "$?" -ne 0 ]] ; then
   echo 'mvn install failed. Exited with non-zero value '; exit 42
 fi
+
+echo 'test1'
+
 cp lib/*.jar target/zenvisage/WEB-INF/lib/.
+
+echo 'test2'
 
 # run junit tests
 sh scripts/run-test.sh
+
+echo 'test3'
 sudo sh run.sh
+echo 'test4'
  # give zenvisage some time to start
 sleep 10
 
