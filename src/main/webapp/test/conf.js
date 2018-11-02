52bc39d14a8c86167b3f684b74e01957d6007f0b
Jaewoo Kim <jaewookim@host-4-30.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-11-02 01:02:17 -0500

    added certs

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -2,4 +2,17 @@ exports.config = {
   framework: 'jasmine',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['query.spec.js','basicCheck.spec.js','dynamicClass.spec.js']
+
+multiCapabilities:[
+{
+'browserName': 'internet explorer',
+},
+{
+'browserName' : 'chrome',
+},
+{
+'browserName' : 'firefox',
+},
+]
+
 }
