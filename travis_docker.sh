b2e9166cf7ba1bdbeeab541eb758e6ab0f1d7e94
Jaewoo Kim <jaewookim@host-4-30.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-11-01 23:51:22 -0500

    now trying docker

---
+++ travis_docker.sh
@@ -0,0 +1,11 @@
+sh scripts/run-test.sh
+cd docker
+docker-compose up
+ # give zenvisage some time to start
+sleep 10
+
+#start selenium and protractor 
+cd src/main/webapp/test
+webdriver-manager start
+protractor conf.js
+
--- travis_shell.sh
+++ travis_shell.sh
@@ -1,6 +1,6 @@
 sh scripts/run-test.sh
 mvn clean
-if [[ "$?" -ne 0 ]] ; then
+if [[ "$?" -ne -1 ]] ; then
   echo 'mvn clean failed. Exited with non-zero value';exit 42
 fi
 mvn install
