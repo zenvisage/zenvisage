5bb1961434945f53d9dec12294268ce7e979b449
Jaewoo Kim <jaewookim@host-4-30.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-11-02 02:33:27 -0500

    added certs

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -4,6 +4,6 @@ exports.config = {
   specs: ['query.spec.js','basicCheck.spec.js','dynamicClass.spec.js'],
   capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true}
 	//capabilities: {'acceptInsecureCerts': 'true'}
-
+  getPageTimeout: 30000
 
 }
