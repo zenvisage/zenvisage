5b2cbab6f034d14c9545e95ec482b71e92cd7a76
dorisjlee <dorisjunglinlee@gmail.com>
2018-11-02 15:25:03 -0500

    test data smoothing on frontend. discovered that issue #128 still unresolved.

--- src/main/webapp/test/basicCheck.spec.js
+++ src/main/webapp/test/basicCheck.spec.js
@@ -1,21 +1,13 @@
 var Utils = require('./testUtils.js');
 var utils = new Utils();
 
-
 describe('Basic Checks', function() {
-  it('Title Equality Test', function() {
+  describe('Title Equality Test', function() {
     utils.initialize()
     expect(browser.getTitle()).toEqual('Zenvisage');
   });  
- //  it("Data Smoothing",function(){
- //   utils.initialize()
-  // var sliderBar = element.all(by.css("#slider-range-max"));
- //    browser.actions().dragAndDrop(sliderBar,{x:100,y:0}).perform(); 
- //    browser.sleep(10000);
- //    console.log(sliderBar.getAttribute('value'))
- //    // expect(sliderBar.getAttribute('value')).toEqual('7');
- //  });
-   it("Switch to different datasets and ensuring basic checks satisfied",function(){
+
+  describe("Switch to different datasets and ensuring basic checks satisfied",function(){
     utils.initialize();
     // console.log(element.all(by.css('#dataset-form-control option')).count());
     var allOptions = element.all(by.css('#dataset-form-control option'))
@@ -40,28 +32,7 @@ describe('Basic Checks', function() {
       })
     }
   });
-  ////////////////////////////////////////////////////////////
-  //// Test Oracle as Self (Visual Examination Hard Coded)////
-  ////////////////////////////////////////////////////////////
-
-  // // Testing a basic interaction-independent function (TODO: Trouble with importing package)
-  // it('should succeed', function() {
-  //   // var Helper = require("./js/helper.js");
-  //   var arr = separateTwoArrays([[1,2,3],[2,2,3]])
-  //   // expect(arr).toEqual([[[1,2][2,2]],[[1,3][2,3]]]);
-  //   expect(arr).toEqual([[[1,2],[2,2]],[[1,3],[2,3]]]);
-  //   // expect(arr).toEqual([1,2,3]);
-  // });
-
-  /////////////////////////////////////////////////////
-  ////Potentially tests to be ran every single time////
-  /////////////////////////////////////////////////////
-  it('Check last similarity above cutoff', function() {
-    // TODO: Vary similarity cutoff, check that the last ranked item is larger or equal to the similarity cutoff
-    expect(1).toEqual(1)
-  });
-
-  it('Check that score@rank n > score@rank m, where n<m.', function() {
+  describe('Check that score@rank n > score@rank m, where n<m.', function() {
     utils.initialize(); 
     checkDescendingResultScore();
   });    
---
+++ src/main/webapp/test/dataSmoothing.spec.js
@@ -0,0 +1,46 @@
+var Utils = require('./testUtils.js');
+var utils = new Utils();
+
+describe('Data Smoothing', function() {
+  // Since the Vary all test takes longer than 60 seconds (original jasmine.DEFAULT_TIMEOUT_INTERVAL) to run, we extend this to 1000 second
+  var originalTimeout;
+
+  beforeEach(function() {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+  });
+
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+  it("Vary all Option + Sliders",function(){
+    utils.initialize();
+    var valLst  = ["movingaverage","exponentialmovingaverage","gaussian"]//"leossInterpolation",
+    var sliderBar = element.all(by.css("#slider-range-max"));    
+    
+    for (var i = valLst.length - 1; i >= 0; i--) {
+      utils.selectDropdownItem("#smoothing-form-control",valLst[i])
+      for (var svar = 0 ; svar <=1; svar +=0.1){
+        browser.executeScript("$('#slider-range-max').slider({value:"+svar +"}); $('#amount').val('"+svar +"') ; scope.callGetUserQueryResultsWithCallBack()");  
+        browser.sleep(3000);
+        // Check that at least the first result, representative, outlier is rendered
+        expect(element(by.id("result-0")).isPresent()).toBe(true);
+        expect(element(by.id("representative-result-0")).isPresent()).toBe(true);
+        expect(element(by.id("outlier-result-0")).isPresent()).toBe(true);
+      }
+    }
+
+    
+    // TODO: Not working drag and drop slider
+    // browser.actions().dragAndDrop(sliderBar,{x:0.9,y:0}).perform(); 
+    // "#slider-range-max > span"
+    // browser.mouseDown(sliderBar).mouseMove({x:0.9,y:0}).mouseUp();
+    // browser.actions()
+    //   .mouseDown(sliderBar)
+    //   .mouseMove({x: 10, y: 0}) 
+    //   .mouseUp()
+    //   .perform()
+    // browser.actions().dragAndDrop(sliderBar,{x:100,y:0}).perform(); 
+    // browser.sleep(10000);
+  })
+});
