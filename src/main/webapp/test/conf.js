62727b5fb4b02345349e2bf279422ef8b23f84c8
Jaewoo Kim <jaewookim@host-4-30.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-11-02 01:17:12 -0500

    added certs

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -3,16 +3,6 @@ exports.config = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['query.spec.js','basicCheck.spec.js','dynamicClass.spec.js']
 
-multiCapabilities:[
-{
-'browserName': 'internet explorer',
-},
-{
-'browserName' : 'chrome',
-},
-{
-'browserName' : 'firefox',
-},
-]
+  capabilities: [{'acceptInsecureCerts': true}]
 
 }
