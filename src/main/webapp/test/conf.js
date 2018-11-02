bff0cd7bc4fd18d924a293ecefcb5aee54a3f069
Jaewoo Kim <jaewookim@host-4-30.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2018-11-02 02:37:19 -0500

    added certs

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -2,7 +2,7 @@ exports.config = {
   framework: 'jasmine',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['query.spec.js','basicCheck.spec.js','dynamicClass.spec.js'],
-  capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true}
+  capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true},
 	//capabilities: {'acceptInsecureCerts': 'true'}
   getPageTimeout: 30000
 
