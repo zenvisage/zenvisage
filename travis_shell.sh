3ba447661275e0ee790c0b764d53f70a414b92a7
dorisjlee <dorisjunglinlee@gmail.com>
2018-11-08 15:17:10 -0600

    changed permission

--- travis_shell.sh
+++ travis_shell.sh
@@ -2,10 +2,12 @@ echo "started travis_shell.sh"
 # mv target/classes/data target/classes/data_cpy
 # cp -r target/classes/data_cpy target/classes/data
 #chmod 666 target/classes/data
+echo "before"
+ls -l target/classes/data
 find target/classes/data -type f -exec chmod 666 {} \;
-ls -l target/classes/
+echo "after"
+ls -l target/classes/data
  
-echo 'test3'
 bash run.sh
  # give zenvisage some time to start
 sleep 10
