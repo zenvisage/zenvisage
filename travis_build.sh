6752a0185b70a96096bfdd0d3b5835869a7ca5fb
dorisjlee <dorisjunglinlee@gmail.com>
2018-11-08 14:46:52 -0600

    mvn clean removed

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,3 @@
-mvn clean
-if [[ "$?" -ne -1 ]] ; then
-  echo 'mvn clean failed. Exited with non-zero value';exit 42
-fi
 mvn install
 if [[ "$?" -ne 0 ]] ; then
   echo 'mvn install failed. Exited with non-zero value '; exit 42
