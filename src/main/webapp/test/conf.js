fe0271debe5ce8f0abe4c9f58634684d315c1120
Jaewoo Kim <jaewookim@host-5-85.ilur711elm.urbana.il.us.clients.pavlovmedia.net>
2018-11-11 03:19:19 -0600

    change conf

--- src/main/webapp/test/conf.js
+++ src/main/webapp/test/conf.js
@@ -2,7 +2,11 @@ exports.config = {
   framework: 'jasmine',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['dataSmoothing.spec.js','query.spec.js','basicCheck.spec.js','dynamicClass.spec.js'],
-  capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true},
+  capabilities: {'browserName': 'chrome', 'acceptInsecureCerts': true
+  chromeOptions: {
+     args: [ "--silent","--log-level=3"]
+   }
+},
 	//capabilities: {'acceptInsecureCerts': 'true'}
   getPageTimeout: 30000,
   allScriptsTimeout: 30000
