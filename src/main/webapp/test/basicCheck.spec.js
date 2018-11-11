9136e7fe5a3b565b6a4edb9cfdc45f2ca40bf151
dorisjlee <dorisjunglinlee@gmail.com>
2018-11-11 16:13:56 -0600

    added 3 test

--- src/main/webapp/test/basicCheck.spec.js
+++ src/main/webapp/test/basicCheck.spec.js
@@ -12,31 +12,31 @@ describe('Basic Checks', function() {
     checkDescendingResultScore();
   });    
 
-  // it("Switch to different datasets and ensuring basic checks satisfied",function(){
-  //   utils.initialize()
-  //   // console.log(element.all(by.css('#dataset-form-control option')).count());
-  //   var allOptions = element.all(by.css('#dataset-form-control option'))
-  //   var totalOptionsCount = allOptions.count()
-  //   expect(allOptions.count()).toEqual(5)
-  //   for (i = 2; i <= 5; i++) {
-  //     // Click open the drop down menu
-  //     element(by.id('dataset-form-control')).click();
-  //     //Select the second item on the drop down menu
-  //     element(by.css('#dataset-form-control > option:nth-child('+i+')')).click();
-  //     // Click on the side to close the drop down menu
-  //     browser.actions().mouseMove({x: 100, y: 0}).click().perform(); 
-  //     // trigger on dataset change
-  //     browser.executeScript("var scope = angular.element('#dataset-form-control').scope();scope.onDatasetChange();")
-  //     // .then(function(){
-  //     //   basicChecks()  
-  //     // });
-  //     // Perform all the basic checks after outlier is rendered (rendering order is result --> representative --> outliers)
-  //     var outlier = element(by.id("outlier-result-0"))
-  //     outlier.isPresent().then(function(){
-  //       basicChecks()
-  //     })
-  //   }
-  // });
+  it("Switch to different datasets and ensuring basic checks satisfied",function(){
+    utils.initialize()
+    // console.log(element.all(by.css('#dataset-form-control option')).count());
+    var allOptions = element.all(by.css('#dataset-form-control option'))
+    var totalOptionsCount = allOptions.count()
+    expect(allOptions.count()).toEqual(5)
+    for (i = 2; i <= 5; i++) {
+      // Click open the drop down menu
+      element(by.id('dataset-form-control')).click();
+      //Select the second item on the drop down menu
+      element(by.css('#dataset-form-control > option:nth-child('+i+')')).click();
+      // Click on the side to close the drop down menu
+      browser.actions().mouseMove({x: 100, y: 0}).click().perform(); 
+      // trigger on dataset change
+      browser.executeScript("var scope = angular.element('#dataset-form-control').scope();scope.onDatasetChange();")
+      // .then(function(){
+      //   basicChecks()  
+      // });
+      // Perform all the basic checks after outlier is rendered (rendering order is result --> representative --> outliers)
+      var outlier = element(by.id("outlier-result-0"))
+      outlier.isPresent().then(function(){
+        basicChecks()
+      })
+    }
+  });
 
   function basicChecks(){
     console.log("basic checked")
